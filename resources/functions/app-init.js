2da9bfdf087af050aec71e09ec4be25827868cf2
Alexander Frick <Alex313031@gmail.com>
2023-03-28 02:32:16 -0500

    3.0.2 final

--- resources/functions/app-init.js
+++ resources/functions/app-init.js
@@ -64,7 +64,7 @@ module.exports = () => {
             frameType: "",
             transparencyEffect: "",
             transparencyTheme: "appearance-based",
-            transparencyDisableBlur: false,
+            transparencyDisableBlur: true,
             transparencyMaximumRefreshRate: "",
             streamerMode: false,
             removeUpsell: true,
